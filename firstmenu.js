ef89384c38855b76a74cbb95adfc2fc4db3d7972
court-johnson <court.landry@gmail.com>
2015-10-20 19:53:17 -0500

    Javascript
    Working out code in JavaScript to have menu dropdown

--- firstmenu.js
+++ firstmenu.js
@@ -1,4 +1,13 @@
-function menu() {
-	$(this).onmouseover="expand";
-  $(this).onmouseout="collapse";
-}
+$(document).ready(function() {
+						   
+  $(.menu).mouseenter(function() {
+    $(this).css("background-color","red");
+  });
+  $(.menu).mouseleave(function() {
+    $(this).css("background-color","yellow");
+  });
+  
+});
+
+
+
